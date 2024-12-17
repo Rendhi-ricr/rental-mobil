c7944f52a72820b8faacba4137c9bb4553971db2
rendhi-ricr <rendhirichardo2@gmail.com>
2024-12-18 00:18:21 +0700

    DB TERBARU 18 DESEMBER

--- db_rental.sql
+++ db_rental.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Dec 10, 2024 at 05:54 PM
+-- Generation Time: Dec 18, 2024 at 12:16 AM
 -- Server version: 8.0.30
 -- PHP Version: 7.4.33
 
@@ -24,6 +24,27 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `tabel_histori_transaksi`
+--
+
+CREATE TABLE `tabel_histori_transaksi` (
+  `id_histori_transaksi` int NOT NULL,
+  `id_transaksi` int NOT NULL,
+  `tanggal_dikembalikan` date NOT NULL,
+  `telat_hari` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `total_denda` decimal(10,0) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tabel_histori_transaksi`
+--
+
+INSERT INTO `tabel_histori_transaksi` (`id_histori_transaksi`, `id_transaksi`, `tanggal_dikembalikan`, `telat_hari`, `total_denda`) VALUES
+(1, 3, '2024-12-17', '0', '0');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tabel_kendaraan`
 --
 
@@ -32,7 +53,7 @@ CREATE TABLE `tabel_kendaraan` (
   `nama_kendaraan` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
   `nomber_kendaraan` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
   `tipe_kendaraan` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
-  `harga_perhari` decimal(10,2) NOT NULL,
+  `harga_perhari` decimal(10,0) NOT NULL,
   `gambar` varchar(255) COLLATE utf8mb4_general_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -41,10 +62,10 @@ CREATE TABLE `tabel_kendaraan` (
 --
 
 INSERT INTO `tabel_kendaraan` (`id_kendaraan`, `nama_kendaraan`, `nomber_kendaraan`, `tipe_kendaraan`, `harga_perhari`, `gambar`) VALUES
-(3, 'Mitsubishi Pajero', 'B1234DA', 'SUV', '850000.00', '1733727212_07d5354b10672a81c418.webp'),
-(4, 'Toyota Avanza', 'A4567DT', 'MVP', '500000.00', '1733727277_0edd76ee0fd5da8a60f9.jpg'),
-(5, 'Mitsubishi Xpander', 'T8763TR', 'MVP', '600000.00', '1733727328_4d60caa260e95cfaa13a.jpg'),
-(6, 'Daihatsu Xenia', 'D9281OP', 'MVP', '500000.00', '1733727395_13cdca460c3fed51eba5.jpg');
+(3, 'Mitsubishi Pajero', 'B 1234 DA', 'SUV', '850000', '1733727212_07d5354b10672a81c418.webp'),
+(4, 'Toyota Avanza', 'A 4567 DT', 'MVP', '500000', '1733727277_0edd76ee0fd5da8a60f9.jpg'),
+(5, 'Mitsubishi Xpander', 'T 8763 TR', 'MVP', '600000', '1733727328_4d60caa260e95cfaa13a.jpg'),
+(7, 'Daihatsu Xenia', 'C 1412 GA', 'MVP', '500000', '1733912146_3d1730c727beca48ed7a.jpg');
 
 -- --------------------------------------------------------
 
@@ -58,19 +79,18 @@ CREATE TABLE `tabel_transaksi` (
   `id_kendaraan` int NOT NULL,
   `tglsewa_mulai` date NOT NULL,
   `tglsewa_akhir` date NOT NULL,
-  `total_harga` decimal(10,0) NOT NULL
+  `total_harga` decimal(10,0) NOT NULL,
+  `denda` decimal(10,0) NOT NULL,
+  `keterangan` enum('Mobil Belum Diambil','Mobil Sudah Diambil','Transaksi Selesai') COLLATE utf8mb4_general_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `tabel_transaksi`
 --
 
-INSERT INTO `tabel_transaksi` (`id_transaksi`, `id_user`, `id_kendaraan`, `tglsewa_mulai`, `tglsewa_akhir`, `total_harga`) VALUES
-(1, 5, 6, '0000-00-00', '2024-12-14', '3500000'),
-(2, 5, 3, '0000-00-00', '2024-12-11', '1700000'),
-(3, 5, 4, '0000-00-00', '2024-12-13', '2000000'),
-(4, 5, 4, '2024-12-11', '2024-12-13', '1500000'),
-(5, 5, 4, '2024-12-10', '2024-12-14', '2500000');
+INSERT INTO `tabel_transaksi` (`id_transaksi`, `id_user`, `id_kendaraan`, `tglsewa_mulai`, `tglsewa_akhir`, `total_harga`, `denda`, `keterangan`) VALUES
+(1, 8, 3, '2024-12-20', '2024-12-25', '5100000', '100000', 'Mobil Sudah Diambil'),
+(2, 5, 5, '2024-12-17', '2024-12-21', '3000000', '100000', 'Mobil Belum Diambil');
 
 -- --------------------------------------------------------
 
@@ -95,12 +115,20 @@ CREATE TABLE `tabel_user` (
 INSERT INTO `tabel_user` (`id_user`, `nama`, `email`, `no_hp`, `alamat`, `password`, `role`) VALUES
 (1, 'rendhi', 'rendhirichardo2@gmail.com', '081224210297', 'qwertyuio', '$2y$10$Y05YT/pflTqJqQ8.Utz5x.3cyIIQIEN0POiQSDYo92Oe/GuKhzHPm', 'admin'),
 (3, 'Nada Savana', 'admin123@gmail.com', '', '', '$2y$10$Iwn4f2Z4cH37ohP5nIVbFOvThfWlsVKhDqXzuQfJWkhBClhimxItG', 'admin'),
-(4, 'qwertyuio', 'sdfghjk@dfgh', '', '', '$2y$10$m3ecqC6FICm7HBR/QT94PONQdBGA8LdjJ2ILHf04gGbSME9sD3dAu', 'pelanggan'),
-(5, 'Siti Masitoh', 'sitim@gmail.com', '', 'qwertyui', '$2y$10$w2.tmd/KM2sHYZ9fqh4u.uoww610WTaKjehpuG053WrwhlhdxNGxS', 'pelanggan');
+(5, 'Siti Masitoh', 'sitim@gmail.com', '', 'iutdfcvnbjhhv', '$2y$10$w2.tmd/KM2sHYZ9fqh4u.uoww610WTaKjehpuG053WrwhlhdxNGxS', 'pelanggan'),
+(6, 'Agus', 'agus@gmail.com', '', '', '$2y$10$Hi0o8p6pkD7C14TmzELGDeytTjYuw630VCjVcYBGzKc9ZdeoepzCK', 'pelanggan'),
+(7, 'Asep', 'asep@gmail.com', '', 'wertyui', '$2y$10$e9g2R/Xqcv7GEUIJXz1/qexhAS1pzYMG9t91MLq0uIxIp3L1HUwpm', ''),
+(8, 'Agung Pakmen', 'agung@example.com', '', 'uiyyfhgvbjhklgv', '$2y$10$umitaw54XQ5lZUiqsP.L7eyHG.lNSsfLr0NvjfgQNvizJkfMOuaj2', 'pelanggan');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `tabel_histori_transaksi`
+--
+ALTER TABLE `tabel_histori_transaksi`
+  ADD PRIMARY KEY (`id_histori_transaksi`);
 
 --
 -- Indexes for table `tabel_kendaraan`
@@ -125,22 +153,28 @@ ALTER TABLE `tabel_user`
 --
 
 --
+-- AUTO_INCREMENT for table `tabel_histori_transaksi`
+--
+ALTER TABLE `tabel_histori_transaksi`
+  MODIFY `id_histori_transaksi` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT for table `tabel_kendaraan`
 --
 ALTER TABLE `tabel_kendaraan`
-  MODIFY `id_kendaraan` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id_kendaraan` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `tabel_transaksi`
 --
 ALTER TABLE `tabel_transaksi`
-  MODIFY `id_transaksi` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_transaksi` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `tabel_user`
 --
 ALTER TABLE `tabel_user`
-  MODIFY `id_user` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_user` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
